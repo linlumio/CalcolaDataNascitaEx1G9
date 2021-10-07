30df62a237b67ccbf9bca3b5527b5281aa42e695
Ale <akip-core@outlook.com>
2021-10-07 14:48:54 +0200

    logic ok, function ok need color and style changes, but not now

--- app/src/main/java/com/example/calcoladatanascitaex1g9/MainActivity.kt
+++ app/src/main/java/com/example/calcoladatanascitaex1g9/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.calcoladatanascitaex1g9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.calcoladatanascitaex1g9.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        var act:ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(act.root)
+
+
+        println("Avvio")
+
+        var coso: Button = findViewById(R.id.calcola)
+
+        coso.setOnClickListener{
+            println("click con il vecchio")
+        }
+
+        act.calcola.setOnClickListener {
+            println("click")
+            var anno = act.date.text.toString().toInt()
+            println("salvato")
+            var annoAttuale = Calendar.getInstance().get(Calendar.YEAR);
+            println("calendario")
+            act.result.text = (annoAttuale-anno).toString()
+
+
+        }
+
     }
 }
